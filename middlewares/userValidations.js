8b563ffc80f48aab53be060d73eb3fe7cfc4bb6a
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-22 16:49:23 -0300

    name validation added

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -2,7 +2,11 @@ const { body } = require("express-validator");
 
 const userCreateValidation = () => {
   return [
-
+    body("name")
+      .isString()
+      .withMessage("Name is required")
+      .isLength({ min: 3 })
+      .withMessage("The name must be at least 3 characters long"),
   ];
 };
 
