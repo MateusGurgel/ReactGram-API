5a2bfafeaa5835ae974b3db75904dea449fb9dc5
Mateus <mateusgurgel15@gmail.com>
2023-02-23 09:37:20 -0300

    register added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,9 +13,36 @@ const generateToken = (id) => {
 
 const register = async (req, res) => {
 
-    
+    const {name, email, password} = req.body
 
-    res.send("Register");
+
+    const user = await User.findOne({email})
+
+    if(user){
+      res.status(422).json({errors: ["ReactGram account already exists"]})
+      return
+    }
+
+
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: passwordHash
+    })
+
+    if(!newUser) {    
+      res.status(422).json({errors: ["An error occurred, please try again later"]})
+      return;
+    }
+
+    
+    res.status(201).json({
+      _id: newUser._id,
+      token: generateToken(newUser._id)
+    })
 };
 
 module.exports = {
