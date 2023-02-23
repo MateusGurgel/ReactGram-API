27dec71d7299fb80a57f4699882e704045659820
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-23 15:42:46 -0300

    update method added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -74,7 +74,41 @@ const getCurrentUser = async (req, res) => {
 };
 
 const update = async(req, res) => {
-  res.send("Brabo")
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+    "-password"
+  );
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
 }
 
 module.exports = {
