99a157a6810a706578c92480bf760f103d0a357f
Mateus <mateusgurgel15@gmail.com>
2023-02-23 11:32:31 -0300

    update path added

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const {register, login, getCurrentUser} = require("../controllers/UserController");
+const {register, login, getCurrentUser,  } = require("../controllers/UserController");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation")
 const authGuard = require("../middlewares/authGuard");
-const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
+const { userCreateValidation, loginValidation, userUpdateValidation } = require("../middlewares/userValidations");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
+router.put("/update", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"))
 
 module.exports = router;
