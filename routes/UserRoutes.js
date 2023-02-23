e2d9bac40b13888e830d1ccd504e162b811bcdbf
Mateus <mateusgurgel15@gmail.com>
2023-02-23 09:55:17 -0300

    login route added

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const {register} = require("../controllers/UserController")
+const {register, login} = require("../controllers/UserController")
 
 //Middlewares
 const validate = require("../middlewares/handleValidation")
-const { userCreateValidation } = require("../middlewares/userValidations")
+const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
 
 module.exports = router;
