973572ea32d9fd1ddb7816e8605e293a83679ffd
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-23 14:05:02 -0300

    update validation fixed

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -37,13 +37,11 @@ const loginValidation = () => {
 const userUpdateValidation = () => {
   return [
     body("name")
-    .isString()
-    .withMessage("Name is required")
+    .optional()
     .isLength({ min: 3 })
     .withMessage("The name must be at least 3 characters long"),
   body("password")
-    .isString()
-    .withMessage("Password is required")
+    .optional()
     .isLength({ min: 5 })
     .withMessage("The password must be at least 5 characters long"),
   ]
