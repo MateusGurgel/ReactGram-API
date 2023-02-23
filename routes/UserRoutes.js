33c90144c2dfa4379780ed55988930cbe2abcf50
Mateus <mateusgurgel15@gmail.com>
2023-02-23 10:55:41 -0300

    auth guard route added

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const {register, login} = require("../controllers/UserController")
+const {register, login, getCurrentUser} = require("../controllers/UserController");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation")
+const authGuard = require("../middlewares/authGuard");
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router;
