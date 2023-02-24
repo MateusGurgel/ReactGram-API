319fe87ba19153ca799b4f72b4e28663e6001a22
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-24 17:19:42 -0300

    photoInsertValidation route added

--- routes/PhotoRoutes.js
+++ routes/PhotoRoutes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
+const { insertPhoto } = require("../controllers/PhotoController")
 
 //Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
-const validate = require("../middlewares/handleValidator")
-// routes
+const validate = require("../middlewares/handleValidation")
+const { imageUpload } = require("../middlewares/imageUpload")
 
+//Routes
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto)
 
 module.exports = router;
