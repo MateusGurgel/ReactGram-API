942c6b91ae77b36021cb2db745f6bd287823ac2e
Mateus <mateusgurgel15@gmail.com>
2023-02-28 11:51:49 -0300

    cor origin changed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }))
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
